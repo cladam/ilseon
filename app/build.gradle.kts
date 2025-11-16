87571fecafc95e50076c4e0d4ee1de9885f64e67
Claes Adamsson <claes.adamsson@ingka.ikea.com>
2025-11-16 12:49:33 +0100

    ci: bumping internal versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.ilseon"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

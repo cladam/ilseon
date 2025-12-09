3130670597ffce3e300c66bb8bf818f1e4dec43e
Claes Adamsson <claes.adamsson@ingka.ikea.com>
2025-12-09 14:58:08 +0100

    fix: push new features to Play Store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "com.ilseon"
         minSdk = 24
         targetSdk = 36
-        versionCode = 56
-        versionName = "0.21.7"
+        versionCode = 57
+        versionName = "0.22.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

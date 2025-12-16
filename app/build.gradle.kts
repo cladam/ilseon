1f9e49d29f33494d28a2a71926d71faf546d96c3
Claes Adamsson <claes.adamsson@ingka.ikea.com>
2025-12-16 09:53:06 +0100

    fix: push new release for transcriptions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "com.ilseon"
         minSdk = 24
         targetSdk = 36
-        versionCode = 74
-        versionName = "0.24.5"
+        versionCode = 75
+        versionName = "0.25.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
